0e9560a512418ccf39197a2d4498571dd2058b8a
Brandon Starcheus <bstarcheus@gmail.com>
2020-06-27 16:51:44 -0400

    Update footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,17 +4,10 @@ const Footer = () => (
   <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">About Us</h2>
-        <p className="mt-5">Ridiculus mus mauris vitae ultricies leo integer malesuada nunc.</p>
-      </div>
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Important Links</h2>
+        <h2 className="text-lg font-semibold">Contact Us</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://codebushi.com">Terms &amp; Conditions</a>
-          </li>
-          <li>
-            <a href="https://codebushi.com">Privacy Policy</a>
+            <a target='_blank' href="mailto:contact@makeuc.io">Email</a>
           </li>
         </ul>
       </div>
@@ -22,13 +15,16 @@ const Footer = () => (
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://dev.to/changoman">Dev.to</a>
+            <a target='_blank' href="https://www.linkedin.com/company/makeuc/">LinkedIn</a>
           </li>
           <li>
-            <a href="https://twitter.com/HuntaroSan">Twitter</a>
+            <a target='_blank' href="https://www.instagram.com/uc_ieee">Instagram</a>
           </li>
           <li>
-            <a href="https://github.com/ieeeatuc/makeuc.io">GitHub</a>
+            <a target='_blank' href="https://twitter.com/uc_ieee">Twitter</a>
+          </li>
+          <li>
+            <a target='_blank' href="https://github.com/ieeeatuc/makeuc.io">GitHub</a>
           </li>
         </ul>
       </div>
