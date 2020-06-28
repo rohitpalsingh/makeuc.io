4c1cacd1b276644bca3b9d719fd4079af95112d5
Brandon Starcheus <bstarcheus@gmail.com>
2020-06-28 16:27:29 -0400

    Add footer img and icons

---
+++ src/components/FooterImage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import footerimg from '../../images/footer.png';
+
+const FooterImage = () => (
+    <img src={footerimg}></img>
+);
+export default FooterImage;
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,35 +1,31 @@
 import React from 'react';
+import FooterImage from '../FooterImage';
+import {FaEnvelope, FaLinkedin, FaInstagram, FaTwitter, FaGithub} from 'react-icons/fa';
+import {IconContext} from 'react-icons';
 
 const Footer = () => (
-  <footer className="container mx-auto py-16 px-3 mt-48 mb-8 text-gray-800">
-    <div className="flex -mx-3">
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Contact Us</h2>
-        <ul className="mt-4 leading-loose">
-          <li>
-            <a target='_blank' href="mailto:contact@makeuc.io">Email</a>
-          </li>
-        </ul>
-      </div>
-      <div className="flex-1 px-3">
-        <h2 className="text-lg font-semibold">Social Media</h2>
-        <ul className="mt-4 leading-loose">
-          <li>
-            <a target='_blank' href="https://www.linkedin.com/company/makeuc/">LinkedIn</a>
-          </li>
-          <li>
-            <a target='_blank' href="https://www.instagram.com/uc_ieee">Instagram</a>
-          </li>
-          <li>
-            <a target='_blank' href="https://twitter.com/uc_ieee">Twitter</a>
-          </li>
-          <li>
-            <a target='_blank' href="https://github.com/ieeeatuc/makeuc.io">GitHub</a>
-          </li>
-        </ul>
-      </div>
-    </div>
+  <>
+  <FooterImage />
+  <footer className="container mx-auto py-16 px-3 mb-8 text-gray-800 flex">
+    <IconContext.Provider value={{ size:'2em', className:"mx-auto" }}>
+      <a target="_blank" href="mailto:contact@makeuc.io" className="flex-1">
+        <FaEnvelope />
+      </a>
+      <a target='_blank' href="https://www.linkedin.com/company/makeuc" className="flex-1">
+        <FaLinkedin />
+      </a> 
+      <a target='_blank' href="https://www.instagram.com/uc_ieee" className="flex-1">
+        <FaInstagram />
+      </a>
+      <a target='_blank' href="https://twitter.com/uc_ieee" className="flex-1">
+        <FaTwitter />
+      </a>
+      <a target='_blank' href="https://github.com/ieeeatuc/makeuc.io" className="flex-1">
+        <FaGithub />
+      </a>
+    </IconContext.Provider>
   </footer>
+  </>
 );
 
 export default Footer;
