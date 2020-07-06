522f393ab8d36badd556ec7c6695cdb72995ebdf
Dev Agrawal <devagrawal09@gmail.com>
2020-07-06 02:18:17 -0400

    added already verified page

--- src/pages/already-verified.js
+++ src/pages/already-verified.js
@@ -11,7 +11,7 @@ export default () => (
       <section className="pt-20 md:pt-40">
         <div className="container mx-auto px-8 lg:flex">
           <div className="text-center lg:text-left">
-            <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">Thank you for verifying your email!</h1>
+            <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">You have already verified your email!</h1>
             <p className="text-xl lg:text-3xl mt-6 font-light">
               We are looking forward to meeting you virtually on October 3-4, 2020
             </p>
