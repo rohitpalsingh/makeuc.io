95547760d0036303e3f191e29ff85ebc56faff7f
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-07-22 00:47:41 -0400

    Implement lazy loading using gatsby-image

--- src/components/FooterImage.js
+++ src/components/FooterImage.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import footerimg from '../../images/footer.png';
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const FooterImage = () => (
-    <img src={footerimg}></img>
-);
-export default FooterImage;
+const FooterImg = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "footer.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 1500) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+}
+
+export default FooterImg
--- src/components/HeaderImage.js
+++ src/components/HeaderImage.js
@@ -1,7 +1,21 @@
-import React from 'react';
-import Headerimg from '../../images/header.png';
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
-const HeaderImage = () => (
-    <img src={Headerimg}></img>
-);
-export default HeaderImage;
+const HeaderImg = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      placeholderImage: file(relativePath: { eq: "header.png" }) {
+        childImageSharp {
+          fluid(maxWidth: 750) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  `)
+
+  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+}
+
+export default HeaderImg
