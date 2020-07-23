e9e7675bd4e3952e150b55c38aeb6fb6c8e086b5
omaralsayed <42531544+omaralsayed@users.noreply.github.com>
2020-07-23 03:30:01 -0400

    Add alt and remove unused import

--- src/components/ScrollTop.js
+++ src/components/ScrollTop.js
@@ -166,7 +166,10 @@ class ScrollTop extends Component {
         width: '60px',
         padding: `10px 15px`
       }}
-    ><img src={this.state.isFlying ? this.props.litSrc : this.props.hoveringSrc} /></div>
+    ><img 
+    src={this.state.isFlying ? this.props.litSrc : this.props.hoveringSrc} 
+    alt="Btn"
+    /></div>
   }
 
   render() {
