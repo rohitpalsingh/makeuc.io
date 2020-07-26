967948ce4b86f967eaf71a5bd110434446b4e536
Dev Agrawal <devagrawal09@gmail.com>
2020-07-26 00:43:38 -0400

    added gtag script

--- src/components/SEO.js
+++ src/components/SEO.js
@@ -23,6 +23,15 @@ export default () => {
     <title>{title}</title>
     <meta name="description" content={description} />
     <link rel="icon" href="/favicon.ico" type="image/x-icon"></link>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-173527139-1"></script>
+    <script>
+      {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'UA-173527139-1');
+      `}
+    </script>
     <html lang="en" />
   </Helmet>;
 };
