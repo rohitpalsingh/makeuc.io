359f0eef6fb90f49bff6b34089f1879a9e791411
arnavkomaragiri <arnav.komaragiri@gmail.com>
2021-09-13 18:31:46 -0400

    did cool stuff
    omar asked for different commit names so here we are

--- src/pages/index.js
+++ src/pages/index.js
@@ -225,7 +225,7 @@ export default () => {
               <Card className="nes-container is-centered is-rounded m-4 bg-white">
                 {sponsors.map((category, index) =>
                   <>
-                    {!!index && index != 4 && <hr className="mt-5"></hr>} {/* this is absolutely disgusting */}
+                    {!!index && index != 5 && index != 1 && <hr className="mt-5"></hr>} {/* this is absolutely disgusting */}
 
                     <div className="sm:mx-auto px-auto flex justify-center items-center content-around flex-wrap">
                       {category.map((sponsor) => (
